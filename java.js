6ed9db3e10acdb2165d04b624a8bd1e064c37afd
João Vitor <106831985+JoaoCorsete@users.noreply.github.com>
2024-09-17 16:25:39 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
-alert("olha o Gojo,o personagem mais FODA!");
+alert("vamos testar a sua sorte");
 let satoroGojo =  21;
 let resposta = prompt("escolha um número de 1 a 100")
- 
+if(resposta== satoroGojo){
+    console.log("Para Bens(21)")
+}
