4698cb51cc2ff2a02a6c6caead263cdec175fd95
João Vitor <106831985+JoaoCorsete@users.noreply.github.com>
2024-10-07 12:43:53 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -4,19 +4,20 @@ let resposta;
 let tentativas =1;
 
 while (resposta != satoroGojo){
-    resposta = prompt("escolha um número de 1 a 1000000000")
+    resposta = prompt("escolha um número de 1 a 1000000000");
 
 if(resposta== satoroGojo){
-    console.log(`Para Bens ${satoroGojo}`)
+    console.log(`Para Bens ${satoroGojo} com ${satoroGojo} com ${tentativas}`); 
 }
 else{
    if(satoroGojo> resposta){
-       alert(`o numero é maior que ${resposta}`)
+       alert(`o numero é maior que ${resposta}`);
    }
 
    else {
-       alert(`o numero é menor que ${resposta}`)
+       alert(`o numero é menor que ${resposta}`);
    }
 
 }
+
 }
